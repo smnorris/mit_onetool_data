7a2a459c166fd589da5a9b038beba18ce4b0fc35
Simon Norris <snorris@hillcrestgeo.ca>
2015-03-26 18:07:14 -0700

    pointed lookup generation query to latest communities table and fixed column names

--- Government/src/electoral_area_lookups.sql
+++ Government/src/electoral_area_lookups.sql
@@ -6,12 +6,12 @@ SELECT * FROM
  c.census_subdivision_id,
  c.clab_id,
  c.community_name,
- p.bc_electoral_district_id AS prov_electoral_district_id,
- p.electoral_district_name AS prov_electoral_district_name,
+ p.bc_electoral_district_id AS prov_riding_id,
+ p.electoral_district_name AS prov_riding_name,
  SUM(st_area(c.geom)) / 10000 AS community_ha,
  SUM(st_area(st_intersection(c.geom, p.geom))) / 10000 AS overlap_ha,
  SUM(st_area(st_intersection(c.geom, p.geom))) / SUM(st_area(c.geom)) AS overlap_pct
-FROM mit.communities c
+FROM mit.communities_ply c
 INNER JOIN
 whse_admin_boundaries.ped_prov_electoral_dist_poly p
 ON st_intersects(c.geom, p.geom)
@@ -31,12 +31,12 @@ SELECT * FROM
  c.census_subdivision_id,
  c.clab_id,
  c.community_name,
- p.fednum AS federal_electoral_area_number,
- p.enname AS federal_electoral_area_name,
+ p.fednum AS federal_riding_id,
+ p.enname AS federal_riding_name,
  SUM(st_area(c.geom)) / 10000 AS community_ha,
  SUM(st_area(st_intersection(c.geom, p.geom))) / 10000 AS overlap_ha,
  SUM(st_area(st_intersection(c.geom, p.geom))) / SUM(st_area(c.geom)) AS overlap_pct
-FROM mit.communities c
+FROM mit.communities_ply c
 INNER JOIN mit.federal_electoral_boundary p
 ON st_intersects(c.geom, p.geom)
 GROUP BY 
